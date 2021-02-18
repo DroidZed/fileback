ef74ac462aefe5c63b1c73aaab9a34fe357524c9
DroidZed <aymen.dragon5@gmail.com>
2021-02-18 15:40:57 +0100

    Initial commit

---
+++ src/main/java/com/wevioo/fileback/FilebackApplication.java
@@ -0,0 +1,13 @@
+package com.wevioo.fileback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilebackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilebackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wevioo/fileback/FilebackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wevioo.fileback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilebackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
