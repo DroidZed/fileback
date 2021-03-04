91b5098d4f8ba14477b49239d9df1e216644d2ba
DroidZed <aymen.dragon5@gmail.com>
2021-03-04 16:24:07 +0100

    Did some refactoring

--- src/main/java/com/wevioo/fileback/repository/UserRepository.java
+++ src/main/java/com/wevioo/fileback/repository/UserRepository.java
@@ -3,13 +3,11 @@ package com.wevioo.fileback.repository;
 import com.wevioo.fileback.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    
     User findByUserName(String username);
     User findByEmail(String email);
 
